1fc3e05afe179c069b67158775973050b8c0d235
Cine06 <puzon.304620@caloocan.sti.edu.ph>
2025-09-27 00:43:28 +0800

    fix: correct vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Learnfusion_web/",
   plugins: [react()],
-  base: "/Learnfusion_web",
   server: {
-    port: 5173,
-  },
-  build: {
-    rollupOptions: {
-      external: ['sweetalert2'],
-    },
+    port: 4000,
   },
 });
