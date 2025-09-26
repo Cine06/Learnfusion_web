bb84260ff9acaaf33852c9b849d4ed88c5e2f840
Cine06 <puzon.304620@caloocan.sti.edu.ph>
2025-09-27 00:48:05 +0800

    fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Learnfusion_web",
+  base: "/Learnfusion_web/",
   plugins: [react()],
   server: {
     port: 4000,
