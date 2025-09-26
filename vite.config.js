a636687f6412949c86eef7b32cc625b4f13672d6
Cine06 <puzon.304620@caloocan.sti.edu.ph>
2025-09-27 00:29:32 +0800

    SweetAlert error fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Learnfusion_web",
   server: {
     port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      external: ['sweetalert2'],
+    },
   },
 });
